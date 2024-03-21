c8257c86aeef5ebc06344a957da3c2afcd915e95
Samyak Shakya <shakyasamyak21@gmail.com>
2024-03-21 15:46:28 +0545

    add button to add event

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -73,53 +73,137 @@
 
 "use client";
 import React, { useCallback, useState } from "react";
-import { Box, IconButton } from "@mui/material";
+import { Box, Button, IconButton } from "@mui/material";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { DataGrid } from "@mui/x-data-grid";
 import { useDropzone } from "react-dropzone";
+import Link from "next/link";
 
 const columns = [
   { field: "id", headerName: "ID", width: 90 },
   {
-    field: "firstName",
-    headerName: "First name",
+    field: "title",
+    headerName: "Title",
     width: 150,
     editable: true,
   },
   {
-    field: "lastName",
-    headerName: "Last name",
+    field: "event",
+    headerName: "Event",
     width: 150,
     editable: true,
   },
   {
-    field: "age",
-    headerName: "Age",
+    field: "date ",
+    headerName: "Date",
     type: "number",
-    width: 110,
+    width: 160,
     editable: true,
   },
   {
-    field: "fullName",
-    headerName: "Full name",
+    field: "location",
+    headerName: "Location",
     description: "This column has a value getter and is not sortable.",
     sortable: false,
     width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
   },
 ];
 
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  // { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
+  // { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
+  // { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
+  // { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
+  // { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  // { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  // { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  // { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  // { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  {
+    id: 1,
+    title: "Title",
+    event: "Event",
+    date: "01 / 02 / 2024",
+    location: "Location",
+  },
+  { id: 2, title: "Title", event: "Event", date: "Date", location: "Location" },
+  { id: 3, title: "Title", event: "Event", date: "Date", location: "Location" },
+  { id: 4, title: "Title", event: "Event", date: "Date", location: "Location" },
+  { id: 5, title: "Title", event: "Event", date: "Date", location: "Location" },
+  { id: 6, title: "Title", event: "Event", date: "Date", location: "Location" },
+  { id: 7, title: "Title", event: "Event", date: "Date", location: "Location" },
+  { id: 8, title: "Title", event: "Event", date: "Date", location: "Location" },
+  { id: 9, title: "Title", event: "Event", date: "Date", location: "Location" },
+  {
+    id: 10,
+    title: "Title",
+    event: "Event",
+    date: "Date",
+    location: "Location",
+  },
+  {
+    id: 11,
+    title: "Title",
+    event: "Event",
+    date: "Date",
+    location: "Location",
+  },
+  {
+    id: 12,
+    title: "Title",
+    event: "Event",
+    date: "Date",
+    location: "Location",
+  },
+  {
+    id: 13,
+    title: "Title",
+    event: "Event",
+    date: "Date",
+    location: "Location",
+  },
+  {
+    id: 14,
+    title: "Title",
+    event: "Event",
+    date: "Date",
+    location: "Location",
+  },
+  {
+    id: 15,
+    title: "Title",
+    event: "Event",
+    date: "Date",
+    location: "Location",
+  },
+  {
+    id: 16,
+    title: "Title",
+    event: "Event",
+    date: "Date",
+    location: "Location",
+  },
+  {
+    id: 17,
+    title: "Title",
+    event: "Event",
+    date: "Date",
+    location: "Location",
+  },
+  {
+    id: 18,
+    title: "Title",
+    event: "Event",
+    date: "Date",
+    location: "Location",
+  },
+  {
+    id: 19,
+    title: "Title",
+    event: "Event",
+    date: "Date",
+    location: "Location",
+  },
 ];
 
 const DropzoneComponent = ({ onFileDrop, filePreviews, setFilePreviews }) => {
@@ -157,25 +241,36 @@ const Event = () => {
   const [filePreviews, setFilePreviews] = useState([]);
 
   const handleFileDrop = (files) => {
-    // Handle the dropped files here, for example, you can log them to the console
     console.log("Dropped files:", files);
   };
-
+  const [paginationModel, setPaginationModel] = useState({
+    pageSize: 8,
+    page: 10,
+  });
   return (
     <div
       id="event"
       style={{ height: 400, width: "100%", padding: "20px 0 0 20px" }}>
-      <Box sx={{ height: 400, width: "100%" }}>
+      <div className="add-btn">
+        <Link href="/events/add">
+          <Button variant="contained" sx={{ width: "100px", mb: 2 }}>
+            Add
+          </Button>
+        </Link>
+      </div>
+      <Box sx={{ height: 500, width: 1000 }}>
         <DataGrid
           rows={rows}
           columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: {
-                pageSize: 5,
-              },
-            },
-          }}
+          // initialState={{
+          //   pagination: {
+          //     paginationModel: {
+          //       pageSize: 8,
+          //     },
+          //   },
+          // }}
+          paginationModel={paginationModel}
+          onPaginationModelChange={setPaginationModel}
           pageSizeOptions={[5]}
           checkboxSelection
           disableRowSelectionOnClick
