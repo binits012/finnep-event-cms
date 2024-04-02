34ecba30879418448ceef8b0c90d8c5fd7ba4f1f
Samyak Shakya <shakyasamyak21@gmail.com>
2024-04-02 09:32:59 +0545

    made degin of modal

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -13,13 +13,15 @@ import { MdOutlineEdit, MdOutlineRemoveRedEye } from "react-icons/md";
 import { HiOutlineTrash } from "react-icons/hi";
 import CustomBreadcrumbs from "@/components/CustomBreadcrumbs";
 import Modal from "@/components/Modal";
+import styled from "styled-components";
+import { FaLocationArrow } from "react-icons/fa6";
+import { PulseLoader } from "react-spinners";
 
 const Events = () => {
   const [events, setEvents] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [selectedEvent, setSelectedEvent] = useState(null);
-  const [targetRowId, setTargetRowId] = useState(null);
-  console.log(selectedEvent, "check selected event");
+  // console.log(selectedEvent, "check selected event");
 
   const COLUMNS = [
     {
@@ -81,10 +83,8 @@ const Events = () => {
             style={{ marginLeft: 5, cursor: "pointer" }}
             onClick={() => {
               setShowModal(true);
-              setTargetRowId(row._id);
               setSelectedEvent(row);
               console.log(row, "modal");
-              console.log(targetRowId, "target");
             }}
           />
         </Box>
@@ -132,31 +132,93 @@ const Events = () => {
             initialState={{
               pagination: {
                 paginationModel: {
-                  pageSize: 5,
+                  pageSize: 4,
                 },
               },
             }}
-            pageSizeOptions={[5]}
+            pageSizeOptions={[1, 2, 4]}
             disableRowSelectionOnClick
             getRowId={(row) => row._id}
+            loading={events.length === 0}
+            slots={{
+              loadingOverlay: () => (
+                <div
+                  style={{
+                    height: 300,
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}>
+                  <div style={{ display: "flex", alignItems: "flex-end" }}>
+                    <h2>loading </h2>
+                    <PulseLoader
+                      // color="#ffde59"
+                      color="#000000"
+                      margin={2}
+                      size={7}
+                      speedMultiplier={0.5}
+                    />
+                  </div>
+                </div>
+              ),
+            }}
           />
         </Box>
       </div>
       <Modal
         isVisible={showModal}
-        onClose={() => setShowModal(false)}
-        targetRowId={targetRowId}>
-        <div>
-          {/* <h1>{selectedEvent.eventTitle}</h1>
-          <p>{selectedEvent.eventDescription}</p> */}
-          {/* <p>{selectedEvent.eventTime}</p>
-          <p>{moment(selectedEvent.eventDate).format("MMM DD YYYY")}</p>
-          <p>{selectedEvent.eventPrice}</p>
-          <p>{selectedEvent.occupancy}</p>
-          <p>{selectedEvent.eventLocationAddress}</p>
-          <p>{selectedEvent.eventLocationGeoCode}</p>
-          <p>{selectedEvent.eventPromotionPhoto}</p> */}
-        </div>
+        onClose={() => {
+          setShowModal(false);
+          setSelectedEvent(null);
+        }}>
+        <Styled>
+          <div className="content">
+            <div className="img">
+              <img
+                src={selectedEvent?.eventPromotionPhoto}
+                alt={selectedEvent?.eventTitle}
+                width={500}
+                height={800}
+              />
+            </div>
+            <div className="info">
+              <h1>{selectedEvent?.eventTitle}</h1>
+              <p className="description">
+                Description <span>{selectedEvent?.eventDescription}</span>
+              </p>
+              <div className="date-time">
+                <p className="time">
+                  Time <span>{selectedEvent?.eventTime}</span>
+                </p>
+                <p className="date">
+                  Date
+                  <span>
+                    {moment(selectedEvent?.eventDate).format("MMM DD YYYY")}
+                  </span>
+                </p>
+              </div>
+              <div className="price-ocupancy">
+                <p className="price">
+                  Price
+                  <span>${selectedEvent?.eventPrice["$numberDecimal"]} </span>
+                </p>
+                <p className="occupancy">
+                  Occupancy <span>{selectedEvent?.occupancy}</span>
+                </p>
+              </div>
+              <div className="address-loaction">
+                <p className="address">
+                  Address
+                  <span>{selectedEvent?.eventLocationAddress}</span>
+                </p>{" "}
+                <p className="location">
+                  Location <span>{selectedEvent?.eventLocationGeoCode}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </Styled>
       </Modal>
     </>
   );
@@ -166,3 +228,121 @@ export default Events;
 
 // curl --location 'https://eventapp.finnep.fi/api/event' \
 // --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InllbGxvd0JyaWRnZSIsInJvbGUiOiJzdXBlckFkbWluIiwiaWQiOiI2NWZkZWU5NTg0MWJkNjAyYzkwOGExZTIiLCJpYXQiOjE3MTEyOTAzNzYsImV4cCI6MTcxMTQ2MzE3Nn0.Oje0AWz1v0VW3RRfSy7GlJAOlKhXmYhhkl3uAwnLAFw'
+
+const Styled = styled.div`
+  .content {
+    display: flex;
+    flex-direction: row;
+    padding: 20px;
+  }
+  .info {
+    padding-left: 20px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    h1 {
+      align-self: center;
+      font-size: 45px;
+      margin-bottom: 20px;
+      font-weight: bolder;
+      justify-content: center;
+    }
+    .description {
+      font-size: 16px;
+      margin-bottom: 5px;
+      font-weight: bolder;
+      display: flex;
+      flex-direction: column;
+      span {
+        margin-top: 5px;
+        border-radius: 5px;
+        border: 1px solid #4c4c4c;
+        padding: 5px;
+        font-weight: normal;
+      }
+    }
+    .date-time {
+      display: flex;
+      justify-content: flex-start;
+      flex-direction: row;
+      margin-bottom: 20px;
+      margin-top: 10px;
+      .time {
+        display: flex;
+        flex-direction: column;
+        padding-right: 20px;
+      }
+      .date {
+        display: flex;
+        flex-direction: column;
+        padding-left: 20px;
+      }
+      p {
+        font-weight: bolder;
+        padding: 5px;
+      }
+      span {
+        margin-top: 5px;
+        border-radius: 5px;
+        border: 1px solid #4c4c4c;
+        padding: 5px;
+      }
+    }
+    .price-ocupancy {
+      display: flex;
+      justify-content: flex-start;
+      flex-direction: row;
+      margin-bottom: 20px;
+      margin-top: 10px;
+      .price {
+        display: flex;
+        flex-direction: column;
+        padding-right: 20px;
+      }
+      .occupancy {
+        display: flex;
+        flex-direction: column;
+        padding-left: 20px;
+      }
+      p {
+        font-weight: bolder;
+        padding: 5px;
+      }
+      span {
+        margin-top: 5px;
+        border-radius: 5px;
+        border: 1px solid #4c4c4c;
+        padding: 5px;
+        font-weight: normal;
+      }
+    }
+    .address-loaction {
+      display: flex;
+      justify-content: flex-start;
+      flex-direction: row;
+      margin-bottom: 20px;
+      margin-top: 10px;
+      .address {
+        display: flex;
+        flex-direction: column;
+        padding-right: 20px;
+      }
+      .location {
+        display: flex;
+        flex-direction: column;
+        padding-left: 20px;
+      }
+      p {
+        font-weight: bolder;
+        padding: 5px;
+      }
+      span {
+        margin-top: 5px;
+        border-radius: 5px;
+        border: 1px solid #4c4c4c;
+        padding: 5px;
+        font-weight: normal;
+      }
+    }
+  }
+`;
