39ae6722e5606b42f473c2a397370dc7889e0479
Samyak Shakya <shakyasamyak21@gmail.com>
2024-04-14 13:29:18 +0545

    added  past date diable & more days to go

--- src/components/forms/AddEvent.js
+++ src/components/forms/AddEvent.js
@@ -199,8 +199,15 @@ const AddEvent = ({ editMode }) => {
               formik.values.eventDate
                 ? dayjs(formik.values.eventDate).format("MMMM DD, YYYY hh:mm A")
                 : ""
-            }`}
-          >
+            }${
+              formik.values.eventDate &&
+              dayjs(formik.values.eventDate).isAfter(dayjs())
+                ? `(${dayjs(formik.values.eventDate).diff(
+                    dayjs(),
+                    "days"
+                  )} days to go)`
+                : ""
+            }`}>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
               <Grid container spacing={2}>
                 <Grid item container md={5} direction={"column"}>
@@ -208,6 +215,7 @@ const AddEvent = ({ editMode }) => {
                     Event Date/ Time
                   </FormLabel>
                   <DateTimePicker
+                    disablePast
                     sx={{
                       margin: 0,
                     }}
@@ -446,8 +454,7 @@ const AddEvent = ({ editMode }) => {
               color: "#fff",
               zIndex: (theme) => theme.zIndex.drawer + 1,
             }}
-            open={loading}
-          >
+            open={loading}>
             <CircularProgress color="inherit" />
           </Backdrop>
         </Grid>
