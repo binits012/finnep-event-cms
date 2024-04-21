62818821c23598bc3bf697982913998a118a35f3
Samyak Shakya <shakyasamyak21@gmail.com>
2024-04-21 17:36:25 +0545

    added columns and add user  button

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,13 +1,22 @@
 "use client";
 import apiHandler from "@/RESTAPIs/helper";
 import CustomBreadcrumbs from "@/components/CustomBreadcrumbs";
+import { Box, Button, Chip, Grid, Input } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import { AiOutlineStop } from "react-icons/ai";
+import { IoIosSearch } from "react-icons/io";
+import { RxCross1 } from "react-icons/rx";
+import { PulseLoader } from "react-spinners";
 import { toast } from "react-toastify";
 import styled from "styled-components";
 
 const Users = () => {
+  const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState("");
   const [users, setUsers] = useState([]);
+
   useEffect(() => {
     const getUserDetails = async () => {
       try {
@@ -22,16 +31,25 @@ const Users = () => {
     getUserDetails();
   }, []);
 
+  const handleAskForDelete = async (e, row) => {
+    console.log(row);
+    const res = await apiHandler("DELETE", `/user/${row._id}`, true);
+    console.log(res);
+    if (res.status === 204) {
+      toast.success("User disabled successfully");
+    }
+  };
+
   const COLUMN = [
     {
       field: "name",
-      headerName: "Name",
-      width: 200,
+      headerName: "Username",
+      width: 150,
     },
     {
       field: `role`,
       headerName: "Role",
-      width: 200,
+      width: 70,
       renderCell({ row }) {
         return (
           <div
@@ -42,6 +60,70 @@ const Users = () => {
             }}>
             {row.role.roleType}
           </div>
+        );
+      },
+    },
+    {
+      field: "notificationAllowed",
+      headerName: "Notification",
+      width: 120,
+      renderCell({ row }) {
+        return (
+          <Chip
+            size="medium"
+            label={row.notificationAllowed ? "Allowed" : "Not Allowed"}
+            variant="outlined"
+            style={{
+              display: "flex",
+              width: "100%",
+              borderColor: `${
+                row.notificationAllowed === true ? "green" : "red"
+              }`,
+              color: `${row.notificationAllowed === true ? "green" : "red"}`,
+              fontWeight: "bold",
+            }}
+          />
+        );
+      },
+    },
+    {
+      field: "active",
+      headerName: "Active Status",
+      width: 100,
+      sortable: false,
+      renderCell({ row }) {
+        return (
+          <Chip
+            size="medium"
+            label={row.active ? "Active" : "Inactive"}
+            variant="outlined"
+            style={{
+              display: "flex",
+              width: "100%",
+              borderColor: `${row.active === true ? "green" : "red"}`,
+              color: `${row.active === true ? "green" : "red"}`,
+              fontWeight: "bold",
+            }}
+          />
+        );
+      },
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      renderCell({ row }) {
+        return (
+          <Box>
+            {" "}
+            <AiOutlineStop
+              size={24}
+              color={`${"#C73F33"}`}
+              title="Disable User"
+              style={{ marginLeft: 10, cursor: "pointer" }}
+              // di
+              onClick={(e) => handleAskForDelete(e, row)}
+            />
+          </Box>
         );
       },
     },
@@ -59,19 +141,92 @@ const Users = () => {
           },
         ]}
       />
+      <Grid container justifyContent="flex-end" mb={2}>
+        <Link passHref href="/users/add">
+          <Button variant="contained" onClick={() => setOpen(true)}>
+            + Add User
+          </Button>
+        </Link>
+      </Grid>
       <DataGrid
-        rows={users}
+        rows={users.filter((user) =>
+          user.name.toLowerCase().includes(search.toLowerCase())
+        )}
         columns={COLUMN}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: 4,
+              pageSize: 5,
             },
           },
         }}
-        pageSizeOptions={[1, 2, 4]}
+        pageSizeOptions={[5, 10, 20]}
         getRowId={(row) => row._id}
         disableSelectionOnClick
+        loading={users.length === 0}
+        slots={{
+          toolbar: () => (
+            <Input
+              placeholder="Search Username"
+              value={search}
+              sx={{
+                width: 200,
+                margin: 2,
+                "--Input-focusedInset": "var(--any, )",
+                "--Input-focusedThickness": "0.50rem",
+                "--Input-focusedHighlight": "rgba(13,110,253,.25)",
+                "&::before": {
+                  transition: "box-shadow .15s ease-in-out",
+                },
+                "&:focus-within": {
+                  borderColor: "#86b7fe",
+                },
+              }}
+              onChange={(e) => setSearch(e.target.value)}
+              endAdornment={
+                search && search !== " " ? (
+                  <RxCross1
+                    size={25}
+                    style={{
+                      margin: 8,
+                      cursor: "pointer",
+                    }}
+                    onClick={(e) => setSearch("")}
+                  />
+                ) : (
+                  <IoIosSearch
+                    size={30}
+                    style={{
+                      margin: 8,
+                      cursor: "pointer",
+                    }}
+                  />
+                )
+              }
+            />
+          ),
+          loadingOverlay: () => (
+            <div
+              style={{
+                height: 300,
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}>
+              <div style={{ display: "flex", alignItems: "flex-end" }}>
+                <h2>loading </h2>
+                <PulseLoader
+                  // color="#ffde59"
+                  color="#000000"
+                  margin={2}
+                  size={7}
+                  speedMultiplier={0.5}
+                />
+              </div>
+            </div>
+          ),
+        }}
       />
     </FormWrapper>
   );
